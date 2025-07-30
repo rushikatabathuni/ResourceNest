157eccaea246e11d62909d07a5336acfe084371a
rushikatabathuni <167748487+rushikatabathuni@users.noreply.github.com>
2025-07-30 11:06:14 +0530

    Update tags.py
    change model to paraphrase-MiniLM-L3-v2 for efficient inference to  not exceed 512mb ram

--- backend/ml/tags.py
+++ backend/ml/tags.py
@@ -1,7 +1,7 @@
 # ml/tags.py
 from keybert import KeyBERT
 
-tagger = KeyBERT("all-MiniLM-L6-v2")
+tagger = KeyBERT("paraphrase-MiniLM-L3-v2")
 
 def extract_tags(text: str):
     return [kw[0] for kw in tagger.extract_keywords(text, top_n=5)]
